64e28c56bc14e20192ba77924d920cc1fd64430f
11307703 <11307703@student.pxl.be>
2024-12-15 20:20:33 +0100

    finaal

---
+++ 10 Getallen/10 Getallen/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _10_Getallen
+{
+    internal class Program
+    {
+        private static int[] numbers = new int[10];
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Even getallen uit 10 tonen==");
+            Console.WriteLine();
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Geef getal {i + 1}/10: " );
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("==Even getallen afdrukken==");
+            
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.WriteLine($"Getal {i + 1} is een even getal: {numbers[i]}");
+                }
+            }
+
+        }
+    }
+}
